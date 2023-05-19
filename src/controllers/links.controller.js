eaa94421ea67481739dadd38bf918736c574b273
Diego <diegobecker2007@hotmail.com>
2023-05-19 13:50:03 -0300

    feat: add GET /urls/:id route

--- src/controllers/links.controller.js
+++ src/controllers/links.controller.js
@@ -11,10 +11,23 @@ export async function shortenUrl(req, res) {
             INSERT INTO links (url,"shortUrl","userId")
             VALUES ($1,$2,$3)
             RETURNING id;
-        `,[url, shortUrl, session.userId]);
+        `, [url, shortUrl, session.userId]);
         // console.log(result.rows);
-        res.status(201).send({id:result.rows[0].id, shortUrl});
+        res.status(201).send({ id: result.rows[0].id, shortUrl });
     } catch (error) {
         res.status(500).send(error.message);
+    }
+}
+
+export async function getShortUrlById(req, res) {
+    const { id } = req.params;
+
+    try {
+        const link = await db.query(`SELECT id, "shortUrl", url FROM links WHERE id=$1`,[id]);
+        if(!link.rows[0]) return res.status(404).send("Url not found");
+        
+        res.send(link.rows[0]);
+    } catch (error) {
+        
     }
 }
--- src/routes/links.routes.js
+++ src/routes/links.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { shortenUrl } from "../controllers/links.controller.js";
+import { getShortUrlById, shortenUrl } from "../controllers/links.controller.js";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
 import { urlSchema } from "../schemas/url.schema.js";
 import { validateToken } from "../middlewares/validateToken.middleware.js";
 
 const linksRouter = Router();
 
-linksRouter.post("/urls/shorten", validateSchema(urlSchema), validateToken, shortenUrl)
+linksRouter.post("/urls/shorten", validateSchema(urlSchema), validateToken, shortenUrl);
+linksRouter.get("/urls/:id", validateToken, getShortUrlById);
 
 export default linksRouter;
