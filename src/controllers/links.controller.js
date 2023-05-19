f3b8364f6659d128ea0ef83c889bfe304f12d665
Diego <diegobecker2007@hotmail.com>
2023-05-19 14:25:13 -0300

    feat: add GET /urls/open/:shorturl route

--- src/controllers/links.controller.js
+++ src/controllers/links.controller.js
@@ -23,11 +23,32 @@ export async function getShortUrlById(req, res) {
     const { id } = req.params;
 
     try {
-        const link = await db.query(`SELECT id, "shortUrl", url FROM links WHERE id=$1`,[id]);
-        if(!link.rows[0]) return res.status(404).send("Url not found");
-        
+        const link = await db.query(`SELECT id, "shortUrl", url FROM links WHERE id=$1`, [id]);
+        if (!link.rows[0]) return res.status(404).send("Url not found");
+
         res.send(link.rows[0]);
     } catch (error) {
+        res.status(500).send(error.message);
+    }
+}
+
+export async function openShortUrl(req, res) {
+    const { shortUrl } = req.params;
+
+    try {
+        const link = await db.query(`SELECT * FROM links WHERE "shortUrl"=$1`, [shortUrl]);
+        if (!link.rows[0]) return res.status(404).send("Short url not found");
+
+        await db.query(`
+            UPDATE links
+            SET "visitCount"="visitCount"+1
+            WHERE "shortUrl"=$1
+        `, [shortUrl]);
+
+        const { url } = link.rows[0];
         
+        res.redirect(200,url);
+    } catch (error) {
+        res.status(500).send(error.message);
     }
 }
--- src/routes/links.routes.js
+++ src/routes/links.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getShortUrlById, shortenUrl } from "../controllers/links.controller.js";
+import { getShortUrlById, openShortUrl, shortenUrl } from "../controllers/links.controller.js";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
 import { urlSchema } from "../schemas/url.schema.js";
 import { validateToken } from "../middlewares/validateToken.middleware.js";
@@ -8,5 +8,6 @@ const linksRouter = Router();
 
 linksRouter.post("/urls/shorten", validateSchema(urlSchema), validateToken, shortenUrl);
 linksRouter.get("/urls/:id", getShortUrlById);
+linksRouter.get("/urls/open/:shortUrl", openShortUrl);
 
 export default linksRouter;
